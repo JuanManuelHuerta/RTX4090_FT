35ec9d171c18f5a2808dd925abeb726a7d3b7e98
JuanManuelHuerta <jmhuertany@gmail.com>
2023-10-01 21:09:45 -0400

    v01 first training script

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # RTX4090_FT
+
+
+Implementation of
+
+``
+https://vast.ai/article/Fine-tuning-the-LLaMA-2-model-on-RTX-4090
+``
+
+``
+https://huggingface.co/meta-llama/Llama-2-7b
+``
+
+``
+pip install transformers peft trl bitsandbytes scipy
+``
+
+
+
---
+++ v01.train.sh
@@ -0,0 +1,10 @@
+python trl/examples/scripts/sft_trainer.py \
+    --model_name meta-llama/Llama-2-7b-hf \
+    --dataset_name timdettmers/openassistant-guanaco \
+    --load_in_8bit \
+    --use_peft \
+    --batch_size 8 \
+    --gradient_accumulation_steps 1
+
+
+    
