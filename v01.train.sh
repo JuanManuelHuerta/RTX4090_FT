6e7f696de330afc70b8045f970670f70a7cd75a2
JuanManuelHuerta <jmhuertany@gmail.com>
2023-10-02 22:11:49 -0400

    update accumulation step

--- v01.train.sh
+++ v01.train.sh
@@ -2,9 +2,9 @@ python trl/examples/scripts/sft_trainer.py \
     --model_name meta-llama/Llama-2-7b-hf \
     --dataset_name timdettmers/openassistant-guanaco \
     --load_in_8bit \
+    --log_with wandb \
     --use_peft \
     --batch_size 8 \
-    --gradient_accumulation_steps 1
+    --gradient_accumulation_steps 2
 
 
-    
